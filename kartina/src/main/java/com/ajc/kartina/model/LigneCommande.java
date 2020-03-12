64d8e25a874cf45bef6a85d71ed5991f02668182
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-12 16:10:10 +0100

    add models

---
+++ kartina/src/main/java/com/ajc/kartina/model/LigneCommande.java
@@ -0,0 +1,116 @@
+package com.ajc.kartina.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+
+@Entity
+public class LigneCommande {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "photo_id")
+	private Photo photo;
+
+	private int quantity;
+	private double prixUnitaire;
+
+	@ManyToOne
+	@JoinColumn(name = "commande_id")
+	private EnteteCommande enteteCommande;
+
+	@ManyToOne
+	@JoinColumn(name = "format_id")
+	private Format format;
+
+	@ManyToOne
+	@JoinColumn(name = "finition_id")
+	private Finition finition;
+
+	@ManyToOne
+	@JoinColumn(name = "cadre_id")
+	private Cadre cadre;
+
+	@Version
+	private int version;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Photo getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Photo photo) {
+		this.photo = photo;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrixUnitaire() {
+		return prixUnitaire;
+	}
+
+	public void setPrixUnitaire(double prixUnitaire) {
+		this.prixUnitaire = prixUnitaire;
+	}
+
+	public EnteteCommande getEnteteCommande() {
+		return enteteCommande;
+	}
+
+	public void setEnteteCommande(EnteteCommande enteteCommande) {
+		this.enteteCommande = enteteCommande;
+	}
+
+	public Format getFormat() {
+		return format;
+	}
+
+	public void setFormat(Format format) {
+		this.format = format;
+	}
+
+	public Finition getFinition() {
+		return finition;
+	}
+
+	public void setFinition(Finition finition) {
+		this.finition = finition;
+	}
+
+	public Cadre getCadre() {
+		return cadre;
+	}
+
+	public void setCadre(Cadre cadre) {
+		this.cadre = cadre;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+}
