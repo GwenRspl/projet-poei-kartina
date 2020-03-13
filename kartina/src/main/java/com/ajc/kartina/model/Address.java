426deaabacc16e1ab6d05071d46aed8c28ad9e98
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-13 10:41:38 +0100

    add controllers & fix repositories

--- kartina/src/main/java/com/ajc/kartina/model/Address.java
+++ kartina/src/main/java/com/ajc/kartina/model/Address.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Version;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Address {
 
@@ -18,6 +20,7 @@ public class Address {
 	private String ville;
 	private String Pays;
 
+	@JsonIgnore
 	@OneToOne(mappedBy = "adresse")
 	private User user;
 
--- kartina/src/test/java/com/ajc/kartina/KartinaApplicationTests.java
+++ kartina/src/test/java/com/ajc/kartina/KartinaApplicationTests.java
@@ -1,13 +1,41 @@
 package com.ajc.kartina;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.ajc.kartina.model.Address;
+import com.ajc.kartina.model.User;
+import com.ajc.kartina.repository.UserRepository;
+
 @SpringBootTest
-class KartinaApplicationTests {
+public class KartinaApplicationTests {
+
+	@Autowired
+	public UserRepository repo;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testUserCreate() {
+		Address address = new Address();
+		address.setRue("rue");
+		address.setCodePostal("75356");
+		address.setVille("ville");
+		address.setPays("France");
+
+		User u = new User();
+		u.setAdresse(address);
+		u.setNom("nom");
+
+		repo.save(u);
+		assertTrue(true);
+
 	}
 
 }
