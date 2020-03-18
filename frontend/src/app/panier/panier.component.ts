9db393f0430ca990350fa9d5514b0a0d6f03dbc1
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-18 12:03:14 +0100

    add shopping cart component

--- frontend/src/app/panier/panier.component.ts
+++ frontend/src/app/panier/panier.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Photo } from '../models/photo';
+import { CartStorageService } from '../services/cart-storage.service';
+import { PhotosService } from '../services/photos.service';
 
 @Component({
   selector: 'app-panier',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panier.component.sass']
 })
 export class PanierComponent implements OnInit {
+  faTrash = faTrash;
+  cartMap: Map<Photo, number>;
+  totalPrice: number = 0;
+  tva: number = 0;
 
-  constructor() { }
+  constructor(private _cartService: CartStorageService, private _photoService: PhotosService) { }
 
   ngOnInit(): void {
+    this.cartMap = this._cartService.getCartMap();
+    console.log(this.cartMap);
+
+    this.cartMap.forEach((value: number, key: Photo) => {
+      this.totalPrice += key.prix;
+      this.tva += key.prix - (key.prix / 1.2);
+    });
+  }
+
+  removeFromCart(photo: Photo) {
+    this._cartService.deleteFromCart(photo);
+  }
+
+  checkout() {
+    console.log('commande passée');
   }
 
 }
